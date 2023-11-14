246896037798e80c8a51d171a9291b9fced641f6
nmassaro <nicole@hiivemarkets.com>
2023-11-14 17:15:38 -0500

    feat: initial Word module

--- words/lib/words/game/word.ex
+++ words/lib/words/game/word.ex
@@ -1,10 +1,20 @@
 defmodule Words.Game.Word do
-  def new(_answer, _guess) do
+  def new(answer, guess) do
+    answer_graphemes = String.graphemes(answer)
+    guess_graphemes = String.graphemes(guess)
 
+    Enum.zip(answer_graphemes, guess_graphemes)
+    |> mark_greens()
   end
 
-  def show(_list) do
+  defp mark_greens(zipped_list) do
+    Enum.map(zipped_list, fn {a, g} ->
+      if a == g, do: {g, :green}, else: {g, :yellow}
+    end)
+  end
 
+  def show(score) do
+    inspect(score)
   end
 
   # IO.ANSI color code the background
